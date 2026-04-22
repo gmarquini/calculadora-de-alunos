895b9702b9a90d33884f56b56ba700e0f30b5f55
gmarquini <gabriel_marquini@hotmail.com>
2026-04-21 22:46:06 -0300

    favicon added

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,13 +6,12 @@ import { StudentFromDB } from './components/StudentsFromDB'
 function App() {
   return (
     <div className="flex h-screen overflow-hidden">
-      <div className="flex-1 min-h-0 overflow-y-auto pr-4">
-        <StudentFromDB />
-      </div>
-
       <div className="mx-10 p-20 flex flex-col bg-blue-400 h-min rounded-2xl items-center shadow-2xl min-w-75 mt-4">
         <Form />
         <CalcStudentList />
+      </div>
+      <div className="flex-1 min-h-0 overflow-y-auto pr-4">
+        <StudentFromDB />
       </div>
     </div>
   )
